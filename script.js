7f0b22b83d1b33a120dd648dc1f3ef289e655721
YashRTech <ramjitiwari4455@gmail.com>
2025-03-19 23:21:49 +0530

    bug fixes

--- script.js
+++ script.js
@@ -1,43 +1,30 @@
 let tl = gsap.timeline();
-tl.from(".logo", {
-  x: -20,
-  y: -50,
-  opacity: 0,
+tl.to(".logo,nav p,.contactus", {
+  x: 0,
+  y: 0,
+  opacity: 1,
   duration: 0.5,
+  stagger:0.3,
   delay: 0.2,
 })
-  .from("nav p", {
-    x: -15,
-    y: -50,
-    opacity: 0,
-    duration: 0.5,
-    stagger: 0.3,
-  })
-  .from(".contactus", {
-    x: 20,
-    y: -50,
-    opacity: 0,
-    duration: 0.5,
-  });
 
-gsap.from(".sub1", {
-  xPercent: -100,
-  opacity: 0,
+gsap.to(".sub1", {
+  transform: "translateX(0)",
+  opacity: 1,
   duration: 1,
 });
 
-gsap.from(".sub2", {
-  x: 50,
-
-  opacity: 0,
-  duration: 0.5,
+gsap.to(".sub2", {
+  x: 0,
+  opacity: 1,
+  duration: 0.3,
   delay: 0.3,
 });
 
-gsap.from(".title", {
-  x: 20,
-  y: -40,
-  opacity: 0,
+gsap.to(".title", {
+  x: 0,
+  y: 0,
+  opacity: 1,
   duration: 0.7,
   delay: 0.2,
 });
@@ -54,11 +41,11 @@ gsap.to(".mainImg,.mainImgWrap", {
   // ease: "power2.out",
 });
 
-gsap.from("#Services div h2", {
-  x: -10,
-  y: -50,
-  opacity: 0,
-  duration: 0.8,
+gsap.to("#Services div h2", {
+  x: 0,
+  y: 0,
+  opacity: 1,
+  duration: 0.3,
   scrollTrigger: {
     trigger: "#Services",
     scroller: "body",
@@ -68,10 +55,10 @@ gsap.from("#Services div h2", {
   },
 });
 
-gsap.from(".project1", {
-  xPercent: -100,
+gsap.to(".project1", {
+  transform: "translateX(0)",
 
-  opacity: 0,
+  opacity: 1,
   duration: 2,
   scrollTrigger: {
     trigger: ".project1",
@@ -82,10 +69,10 @@ gsap.from(".project1", {
     end: "top 50%",
   },
 });
-gsap.from(".project2", {
-  xPercent: 100,
+gsap.to(".project2", {
+  transform: "translateX(0)",
 
-  opacity: 0,
+  opacity: 1,
   duration: 2,
   scrollTrigger: {
     trigger: ".project2",
@@ -96,10 +83,10 @@ gsap.from(".project2", {
     end: "top 30%",
   },
 });
-gsap.from(".s-text", {
-  x: -10,
-  y: -50,
-  opacity: 0,
+gsap.to(".s-text", {
+  x: 0,
+  y: 0,
+  opacity: 1,
   duration: 0.8,
   delay: 0.3,
   scrollTrigger: {
@@ -108,16 +95,17 @@ gsap.from(".s-text", {
     scrub: 1,
   },
 });
-gsap.from("#Projects", {
-  x: -20,
-  y: -30,
-  opacity: 0,
+gsap.to("#Projects", {
+  x: 0,
+  y: 0,
+  duration:0.5,
+  opacity: 1,
   scrollTrigger: {
     trigger: "#Projects",
     scroller: "body",
     scrub: 1,
     start: "20% 100%",
-    end: "50% 10%",
+    end: "20% 20%",
   },
 });
 
@@ -158,10 +146,10 @@ gsap.from(".swiper", {
   },
 });
 
-gsap.from("#Contact div h2", {
-  x: -20,
-  y: -30,
-  opacity: 0,
+gsap.to("#Contact div h2", {
+  x: 0,
+  y: 0,
+  opacity: 1,
   scrollTrigger: {
     trigger: "#Contact",
     scroller: "body",
@@ -171,10 +159,10 @@ gsap.from("#Contact div h2", {
   },
 });
 
-gsap.from("#Contact div div a", {
-  x: -20,
-  y: -30,
-  opacity: 0,
+gsap.to("#Contact div div a", {
+  x: 0,
+  y: 0,
+  opacity: 1,
   stagger: 0.3,
   scrollTrigger: {
     trigger: "#Contact",
@@ -185,10 +173,10 @@ gsap.from("#Contact div div a", {
   },
 });
 
-gsap.from("#Skills div div", {
-  x: -20,
-  y: -30,
-  opacity: 0,
+gsap.to("#Skills div div", {
+  x: 0,
+  y: 0,
+  opacity: 1,
   stagger: 0.8,
   scrollTrigger: {
     trigger: "#Skills",
@@ -202,11 +190,9 @@ gsap.from("#Skills div div", {
 let icon = document.querySelector(".icon");
 let closeIcon = document.querySelector(".closeIcon");
 
-
 let tl2 = gsap.timeline();
 tl2.to(".full", {
-    right: 0,
-   
+  right: 0,
 });
 tl2.from(".full p", {
   x: -50,
@@ -215,14 +201,19 @@ tl2.from(".full p", {
   stagger: 0.3,
 });
 tl2.from(".closeIcon", {
-    opacity: 0,
-    duration:0.3
-    
-})
-tl2.pause()
-icon.addEventListener("click",()=> {
-    tl2.play()
-})
+  opacity: 0,
+  duration: 0.3,
+});
+tl2.pause();
+icon.addEventListener("click", () => {
+  tl2.play();
+
+  // document.querySelector(".full").classList.add("flex")
+  // document.querySelector(".full").classList.remove("hidden")
+});
 closeIcon.addEventListener("click", () => {
-    tl2.reverse()
-})
+  tl2.reverse();
+
+  // document.querySelector(".full").classList.add("hidden")
+  // document.querySelector(".full").classList.remove("flex")
+});
